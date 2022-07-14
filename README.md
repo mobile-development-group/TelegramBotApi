87cfa346745a0d8dc6b32b92f1b7722b8a9af131
Pavel Kravchenko <pavel.zlotarenchuk@gmail.com>
2022-07-14 18:55:38 +0200

    Added usage documentation

--- README.md
+++ README.md
@@ -1,7 +1,52 @@
+# TelegramBotAPI
+
 [![Release](https://jitpack.io/v/mobile-development-group/TelegramBotApi.svg)]
 (https://jitpack.io/#mobile-development-group/TelegramBotApi)
 
 This is clone telegrambotlibrary from ajts.
 (com.ajts.library.telegrambot:telegrambotlibrary:1.0.0)
-
 It stopped connecting through dependencies, and we also couldn’t find the source code on GitHub, so we created a clone.
+
+To add library to your own project:
+###### Step 1. Add the JitPack repository to your build file
+Add it in your root build.gradle at the end of repositories:
+```
+allprojects {
+    repositories {
+	...
+	maven { url 'https://jitpack.io' }
+	}
+}
+```
+###### Step 2. Add the dependency
+```
+dependencies {
+    implementation 'com.github.mobile-development-group:TelegramBotApi:1.0.0'
+}
+```
+###### Step 3. Usage
+
+```
+// For send message
+val telegram = Telegram(BOT_TOKEN)
+telegram.sendMessage(CHAT_ID, message, object : TelegramCallback<Message> {
+    override fun onResponse(call: Call?, response: Message?) {
+        // Successful block
+    }
+
+    override fun onFailure(call: Call?, e: Exception?) {
+        // Error block
+    }
+})
+
+// For send message
+telegram.sendLocation(CHAT_ID, latitude, longitude, telegramCallback)
+
+// For send photo
+telegram.sendPhoto(CHAT_ID, mediaType, file, caption, telegramCallback)
+
+// For send document
+telegram.sendDocument(CHAT_ID, mediaType, file, caption, telegramCallback)
+...
+
+```
--- app/src/main/java/com/mdgroup/telegrambotapiexample/MainActivity.kt
+++ app/src/main/java/com/mdgroup/telegrambotapiexample/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.mdgroup.telegrambotapiexample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
+import com.mdgroup.telegrambotapi.Telegram
+import com.mdgroup.telegrambotapi.models.Message
+import com.mdgroup.telegrambotapi.utils.TelegramCallback
+import okhttp3.Call
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val botToken = findViewById<TextInputEditText>(R.id.botToken)
+        val chatId = findViewById<TextInputEditText>(R.id.chatId)
+        val message = findViewById<TextInputEditText>(R.id.message)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            val telegram = Telegram(botToken.text.toString())
+
+            telegram.sendMessage(
+                chatId.text.toString(),
+                message.text.toString(),
+                object : TelegramCallback<Message> {
+                    override fun onResponse(call: Call?, response: Message?) {
+                        Toast.makeText(this@MainActivity, "Successful", Toast.LENGTH_SHORT).show()
+                    }
+
+                    override fun onFailure(call: Call?, e: Exception?) {
+                        Toast.makeText(this@MainActivity, "Error\n${e?.localizedMessage}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            )
+        }
     }
 }
